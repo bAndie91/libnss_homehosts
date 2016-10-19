d2ed58af91109ea089c80ee56dd2334d912b473e
bAndie91 <bandie9100@gmail.com>
2016-10-19 16:19:33 +0200

    gethostbyaddr support

--- libnss_homehosts.c
+++ libnss_homehosts.c
@@ -19,11 +19,16 @@ struct ipaddr {
 	struct in6_addr ip6;
 };
 
+typedef int bool;
+#define TRUE 1
+#define FALSE 0
 
 #define ALIGN(idx) do { \
   if (idx % sizeof(void*)) \
     idx += (sizeof(void*) - idx % sizeof(void*)); /* Align on 32 bit boundary */ \
 } while(0)
+
+#define AFLEN(af) (((af) == AF_INET6) ? sizeof(struct in6_addr) : sizeof(struct in_addr))
 
 
 int parseIpStr(const char *str, struct ipaddr *addr)
@@ -51,8 +56,14 @@ void* ipaddr_get_binary_addr(struct ipaddr *addr)
 	return NULL;
 }
 
+void seek_line(FILE* fh)
+{
+	/* Seeks to the beginning of next non-empty line on a file. */
+	fscanf(fh, "%*[^\n]%*[\n]");
+}
+
 #ifdef DEBUG
-void printbuffer(unsigned char* buf, size_t len)
+void dumpbuffer(unsigned char* buf, size_t len)
 {
 	unsigned char* p = buf;
 	while(p - buf < len)
@@ -65,12 +76,13 @@ void printbuffer(unsigned char* buf, size_t len)
 }
 #endif
 
-enum nss_status homehosts_gethostbyname_r(
+enum nss_status homehosts_gethostent_r(
 	const char *query_name,
+	const void *query_addr,
 	struct hostent * result,
 	char *buffer,
 	size_t buflen,
-	int *errnop,
+	struct hostent ** result_p,
 	int *h_errnop,
 	int query_af)
 {
@@ -84,11 +96,12 @@ enum nss_status homehosts_gethostbyname_r(
 	char nlbuf[2];		// holds a newline char
 	char *c;
 	int cnt, acnt, tokens;
-	int store_aliases_phase;
+	bool store_aliases_phase, ipaddr_found = FALSE;
 	
-	memset(buffer, ' ', buflen);
+	
 	#ifdef DEBUG
 	warnx("host.conf: inited = %u, flags = %u, multi = %u", _res_hconf.initialized, _res_hconf.flags, (_res_hconf.flags & HCONF_FLAG_MULTI)!=0);
+	memset(buffer, ' ', buflen);
 	#endif
 	
 	/* Open hosts file */
@@ -102,14 +115,18 @@ enum nss_status homehosts_gethostbyname_r(
 	
 	idx = 0;
 	ridx = buflen;		// first byte occupied at the end of buffer
-	strcpy(buffer+idx, query_name);
-	result->h_name = buffer+idx;
-	idx += strlen(query_name)+1;
-	ALIGN(idx);
+	result->h_name = NULL;
+	if(query_name != NULL)
+	{
+		strcpy(buffer+idx, query_name);
+		result->h_name = buffer+idx;
+		idx += strlen(query_name)+1;
+		ALIGN(idx);
+	}
 	addrstart = idx;
 	
 	result->h_addrtype = query_af;
-	result->h_length = (query_af == AF_INET6) ? sizeof(struct in6_addr) : sizeof(struct in_addr);
+	result->h_length = AFLEN(query_af);
 	
 	/* Read hosts file */
 	
@@ -117,17 +134,35 @@ enum nss_status homehosts_gethostbyname_r(
 	acnt = 0;	// Count resulting alias names
 	while(!feof(fh))
 	{
-		if(fscanf(fh, "%s", &ipbuf) == 1)
+		if(fscanf(fh, "%s", (char*)&ipbuf) == 1)
 		{
 			if(ipbuf[0] == '#')
 			{
-				/* Seek to the next line */
-				fscanf(fh, "%*[^\n]%*[\n]");
+				seek_line(fh);
 				continue;
 			}
 			
-			store_aliases_phase = 0;
+			store_aliases_phase = FALSE;
 			aliases_offset = ftell(fh);
+			
+			if(query_addr != NULL)
+			{
+				if(parseIpStr(ipbuf, &address) == 1 
+				   && address.af == query_af 
+				   && memcmp(ipaddr_get_binary_addr(&address), query_addr /* TODO: use struct members */, result->h_length)==0)
+				{
+					ipaddr_found = TRUE;
+					store_aliases_phase = TRUE;
+					cnt++;
+					memcpy(buffer+idx, ipaddr_get_binary_addr(&address), result->h_length);
+					idx += result->h_length;
+				}
+				else
+				{
+					seek_line(fh);
+					continue;
+				}
+			}
 			
 			read_hostname:
 			tokens = fscanf(fh, "%s%1[\n]", namebuf, nlbuf);
@@ -149,16 +184,27 @@ enum nss_status homehosts_gethostbyname_r(
 				
 				if(store_aliases_phase)
 				{
-					if(strcasecmp(namebuf, query_name)!=0)
+					if(query_name == NULL || strcasecmp(namebuf, query_name)!=0)
 					{
-						acnt++;
-						if(idx + strlen(namebuf)+1 /* trailing NUL byte */ + (acnt+1) * sizeof(char*) /* pointers to alias names */ > ridx-1)
+						if(result->h_name == NULL)
 						{
-							fclose(fh);
-							goto buffer_error;
+							/* Save 1st hostname as canonical name */
+							strcpy(buffer+idx, namebuf);
+							result->h_name = buffer+idx;
+							idx += strlen(namebuf)+1;
+							ALIGN(idx);
 						}
-						strcpy(buffer+ridx-strlen(namebuf)-1, namebuf);
-						ridx += -strlen(namebuf)-1;
+						else
+						{
+							acnt++;
+							if(idx + strlen(namebuf)+1 /* trailing NUL byte */ + (acnt+1) * sizeof(char*) /* pointers to alias names */ > ridx-1)
+							{
+								fclose(fh);
+								goto buffer_error;
+							}
+							strcpy(buffer+ridx-strlen(namebuf)-1, namebuf);
+							ridx += -strlen(namebuf)-1;
+						}
 					}
 				}
 				else
@@ -180,7 +226,7 @@ enum nss_status homehosts_gethostbyname_r(
 							idx += result->h_length;
 							
 							/* Treat other hostnames in this line as aliases */
-							store_aliases_phase = 1;
+							store_aliases_phase = TRUE;
 							fseek(fh, aliases_offset, 0);
 							goto read_hostname;
 						}
@@ -190,8 +236,11 @@ enum nss_status homehosts_gethostbyname_r(
 			
 			if(tokens != 1)
 			{
-				/* Encountered newline */
-				if(cnt > 0 && (_res_hconf.flags & HCONF_FLAG_MULTI)==0)
+				/* Encountered a newline */
+				if(cnt > 0 && (ipaddr_found || (_res_hconf.flags & HCONF_FLAG_MULTI)==0))
+					/* Do not continue line reading,
+					   because either address is found or
+					   hostname is found and 'multi off' in host.conf */
 					break;
 				continue;
 			}
@@ -203,7 +252,6 @@ enum nss_status homehosts_gethostbyname_r(
 	
 	if(cnt == 0)
 	{
-		//*errnop = ;
 		*h_errnop = NO_ADDRESS;
 		return NSS_STATUS_NOTFOUND;
 	}
@@ -217,10 +265,10 @@ enum nss_status homehosts_gethostbyname_r(
 		result->h_addr_list[n] = (char*)(buffer + addrstart + n * result->h_length);
 	}
 	result->h_addr_list[n] = NULL;
+	idx += (n+1) * sizeof(char*);
 	
 	/* Store pointers to aliases */
 	
-	idx = addrstart + n * result->h_length + (n+1) * sizeof(char*);
 	result->h_aliases = (char**)(buffer + idx);
 	n = 0;
 	for(; n < acnt; n++)
@@ -236,20 +284,21 @@ enum nss_status homehosts_gethostbyname_r(
 	
 	#ifdef DEBUG
 	warnx("h_name -> %u\nh_aliases -> %u\nh_addrtype = %u\nh_length = %u\nh_addr_list -> %u", (void*)result->h_name - (void*)buffer, (void*)result->h_aliases - (void*)buffer, result->h_addrtype, result->h_length, (void*)result->h_addr_list - (void*)buffer);
-	printbuffer(buffer, buflen);
+	dumpbuffer(buffer, buflen);
 	#endif
 	
+	*result_p = result;
 	return NSS_STATUS_SUCCESS;
 	
 	
 	buffer_error:
-	*errnop = ERANGE;
-	*h_errnop = NO_RECOVERY;
+	*h_errnop = ERANGE;
+	*result_p = NULL;
 	return NSS_STATUS_TRYAGAIN;
 	
 	soft_error:
-	*errnop = EAGAIN;
-	*h_errnop = NO_RECOVERY;
+	*h_errnop = EAGAIN;
+	*result_p = NULL;
 	return NSS_STATUS_TRYAGAIN;
 }
 
@@ -258,14 +307,14 @@ enum nss_status _nss_homehosts_gethostbyname_r(
 	struct hostent * result,
 	char *buffer,
 	size_t buflen,
-	int *errnop,
+	struct hostent ** result_p,
 	int *h_errnop)
 {
-	enum nss_status found_ipv4;
-	found_ipv4 = homehosts_gethostbyname_r(name, result, buffer, buflen, errnop, h_errnop, AF_INET);
-	if(found_ipv4 == NSS_STATUS_NOTFOUND)
-		return homehosts_gethostbyname_r(name, result, buffer, buflen, errnop, h_errnop, AF_INET6);
-	return found_ipv4;
+	enum nss_status found_ipv6;
+	found_ipv6 = homehosts_gethostent_r(name, NULL, result, buffer, buflen, result_p, h_errnop, AF_INET6);
+	if(found_ipv6 == NSS_STATUS_NOTFOUND)
+		return homehosts_gethostent_r(name, NULL, result, buffer, buflen, result_p, h_errnop, AF_INET);
+	return found_ipv6;
 }
 
 enum nss_status _nss_homehosts_gethostbyname2_r(
@@ -274,41 +323,30 @@ enum nss_status _nss_homehosts_gethostbyname2_r(
 	struct hostent * result,
 	char *buffer,
 	size_t buflen,
-	int *errnop,
+	struct hostent ** result_p,
 	int *h_errnop)
 {
-	if (af != AF_INET && af != AF_INET6)
+	if(af != AF_INET && af != AF_INET6)
 	{
-		*errnop = EAFNOSUPPORT;
-		*h_errnop = NO_RECOVERY;
+		*h_errnop = EAFNOSUPPORT;
+		*result_p = NULL;
 		return NSS_STATUS_UNAVAIL;
 	}
 	else
 	{
-		return homehosts_gethostbyname_r(name, result, buffer, buflen, errnop, h_errnop, af);
+		return homehosts_gethostent_r(name, NULL, result, buffer, buflen, result_p, h_errnop, af);
 	}
 }
 
-/*
 enum nss_status _nss_homehosts_gethostbyaddr_r(
 	const void *address,
 	socklen_t len,
 	int af,
-	struct hostent * ret,
+	struct hostent * result,
 	char *buffer,
 	size_t buflen,
-	struct hostent **result,
+	struct hostent ** result_p,
 	int *h_errnop)
 {
+	return homehosts_gethostent_r(NULL, address, result, buffer, buflen, result_p, h_errnop, af);
 }
-*/
-
-struct hostent * _nss_homehosts_gethostbyaddr(
-	const void *addr,
-	socklen_t len,
-	int af)
-{
-	struct hostent result;
-	return NULL;
-}
-
